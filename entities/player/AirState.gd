315a5de3e0a979c8fee8f26f5e4dad77f862172f
cyro0 <chefbigpasta@gmx.net>
2024-07-15 12:06:46 +0200

    player better land

--- entities/player/AirState.gd
+++ entities/player/AirState.gd
@@ -2,25 +2,27 @@ extends State
 
 class_name AirState
 
-@export var landing_state : State
-@export var double_jump_velocity : float = -100
+@export var ground_state : State
+@export var double_jump_velocity : float = -350
 @export var double_jump_animation : String = "double_jump"
 @export var landing_animation : String = "landing"
+var input_after_landing : bool = false
 
 var has_double_jumped = false
 
 func state_process(delta):
 	if(character.is_on_floor()):
-		next_state = landing_state
+		next_state = ground_state
 		
 func state_input(event : InputEvent):
 	if(event.is_action_pressed("jump") && !has_double_jumped):
 		double_jump()
 
 func on_exit():
-	if(next_state == landing_state):
-		playback.travel(landing_animation)
+	if(next_state == ground_state):
 		has_double_jumped = false
+	
+	
 
 func double_jump():
 	character.velocity.y = double_jump_velocity
--- entities/player/GroundState.gd
+++ entities/player/GroundState.gd
@@ -2,19 +2,30 @@ extends State
 
 class_name GroundState
 
-@export var jump_velocity : float = -150.0
+@export var jump_velocity : float = -350.0
 @export var air_state : State
 @export var jump_animation : String = "jump"
+@export var move_animation : String = "Move"
+var ground_loaded_on_init : bool = true
 
 func state_process(delta):
 	if(!character.is_on_floor()):
 		next_state = air_state
 
 func state_input(event : InputEvent):
-	if(event.is_action_pressed("jump")):
+	if (event.is_action_pressed("jump")):
 		jump()
 		
 func jump():
 	character.velocity.y = jump_velocity
 	next_state = air_state
 	playback.travel(jump_animation)
+	
+func on_enter():
+	if (ground_loaded_on_init):
+		ground_loaded_on_init = false
+	else:
+		playback.travel("land")
+
+	
+	
