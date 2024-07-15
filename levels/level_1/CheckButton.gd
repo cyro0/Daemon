4f6b6598bed2e5849564804ada00cb0ba5c6ddbe
cyro0 <chefbigpasta@gmx.net>
2024-07-15 19:20:43 +0200

    audio and changed transition

--- levels/level_1/CheckButton.gd
+++
@@ -1,6 +0,0 @@
-extends CheckButton
-
-@export var transitioner : TransitionToBattleNormal
-
-func _on_toggled(toggled_on):
-	transitioner.set_next_animation(toggled_on)
--- ui/Transition_to_Battle_Normal.gd
+++ ui/Transition_to_Battle_Normal.gd
@@ -6,12 +6,16 @@ class_name TransitionToBattleNormal
 
 @onready var animation_tex : TextureRect = $TextureRect
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var transition_fx = preload("res://audio/battle/Encounter.mp3")
 
 func _ready():
 	animation_tex.visible = false
 	
-func fade_out_and_in():
+func transition():
+	AudioPlayer.play_FX(transition_fx, -12.0)
+	Engine.time_scale = 0.1
 	animation_player.play("fade_out")
+	
 
 
 func _on_animation_player_animation_finished(anim_name):
@@ -19,4 +23,4 @@ func _on_animation_player_animation_finished(anim_name):
 		get_tree().change_scene_to_packed(scene_to_load)
 
 func _on_snail_body_entered(body):
-	fade_out_and_in()
+	transition()
